3a83d53e5f893ab6834803319eb4989b0d7f3aa5
fina <fina@fina-desktop.(none)>
2013-01-26 19:27:25 +0100

    Kosmetyka kodu - grupy

--- Grupa.hs
+++ Grupa.hs
@@ -1,9 +1,8 @@
 module Grupa where
 import IO
---import System.IO.Error
 import Char
 import TextUtil
---import Control.Parallel.Strategies (rnf)
+
 
 grupyPlik = "grupy.dat"
 
@@ -12,10 +11,6 @@ type Name	=	String
 
 data Grupa = Grupa Name deriving (Show,Read,Eq)
 
-								
-
-
-								
 grupaName :: Grupa -> Name
 grupaName (Grupa name) = name
 
@@ -40,8 +35,7 @@ dodajGrupe = do
         do
                         let
                                 
-                                --numerGrupy = read numerGrupyStr :: Int
-                                grupa = Grupa nazwaGrupyStr
+                              grupa = Grupa nazwaGrupyStr
                         if (sprawdzCzyGrupaIstnieje stareGrupy nazwaGrupyStr) then do
                             putStrLn "Podany numer grupy juz istnieje."
                             else do
@@ -59,8 +53,7 @@ modyfikujGrupe = do
                 
                 let grupy = znajdzGrupe stareGrupy nazwaGrupyStr
                 if grupy /= [] then do
-                        --let grupy = grupy !! 0
-                        --putStrLn "Znaleziono grupy:"
+                       
                         putStrLn (grupy2String grupy)
                         
                         zapiszGrupy (usunGrupyZListy stareGrupy nazwaGrupyStr)
@@ -83,19 +76,18 @@ usunGrupe = do
         putStr "Podaj nazwe grupy: "
         grupaNazwaStr <- getLine
         do
-                --let grupaNr = (read grupaNazwaStr) :: Int
+               
                 let grupa = znajdzGrupe stareGrupy grupaNazwaStr
                 if grupa /= [] then do
-                        --let grupy = grupy !! 0
+                       
                         putStrLn "Znaleziono grupe:"
                         putStrLn (grupy2String grupa)
-                        --putStrLn "Czy na pewno chcesz usunac ten stolik? [T/N]"
                         zapiszGrupy (usunGrupyZListy stareGrupy grupaNazwaStr)
                         putStrLn "Grupe usunieto."
                         else do
                         putStrLn "Nie znaleziono grupy o podanym ID."
 
----
+-- wyswietl grupy
 wyswietlGrupy = do
                  listaGrupy <- wczytajGrupy
                  putStrLn(grupy2String listaGrupy)	
@@ -182,7 +174,7 @@ sprawdzIUtworzPlikGrupy = do
                         else
                         putStrLn ("Blad przy otwieraniu pliku: " ++ grupyPlik)
 
-
+-- ilosc grup
 liczGrupy = do
     lista <- wczytajGrupy
     return (length lista)
